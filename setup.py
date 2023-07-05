f21a991b352eae054cdfe794f2f1ca9c8189256e
herve <herve.lecoroller@lam.fr>
2023-07-05 15:20:38 +0200

    cat_sophie replaced by sophiecat in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'cat_sophie = cat_sophie.cat_sophie:main'
+            'sophiecat= sophiecat.sophiecat:main'
         ]
     }
 )
